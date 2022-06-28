2f66c47a2d894d405c34ec07a37ad8e150b65529
kockono <kockonoop@gmail.com>
2022-06-28 15:59:55 -0600

    Sliver

--- 5.5.peliculas/peliculas/lib/screens/details_screen.dart
+++ 5.5.peliculas/peliculas/lib/screens/details_screen.dart
@@ -1,44 +1,91 @@
 import 'package:flutter/material.dart';
-class DetailsScreen extends StatelessWidget {
 
+class DetailsScreen extends StatelessWidget {
   const DetailsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
-    final String movie = ModalRoute.of(context)?.settings.arguments.toString() ?? 'No-movie';
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'No-movie';
 
     return Scaffold(
       body: CustomScrollView(
-        slivers: [ // Son widget que tienen comportamiento preprogramado cuando movemos el padre
-            _CustomAppBar()
+        slivers: [
+          // Son widget que tienen comportamiento preprogramado cuando movemos el padre
+          _CustomAppBar(),
+          SliverList(
+            delegate: SliverChildListDelegate([_PosterAndTitle()]),
+          )
         ],
-       ),
+      ),
     );
   }
 }
 
 class _CustomAppBar extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return  SliverAppBar (
-        backgroundColor: Colors.indigo,
-        expandedHeight: 200,
-        pinned: true, // Que se siga mirando la AppBar
-        flexibleSpace: FlexibleSpaceBar(
-          centerTitle: true,
-          title: Container(
-            width: double.infinity,
-            alignment: Alignment.bottomCenter,
-            child: Text('Movie.title', style: TextStyle(fontSize: 16),),
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 200,
+      pinned: true, // Que se siga mirando la AppBar
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: EdgeInsets.all(2),
+        title: Container(
+          color: Colors.black45,
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          child: const Text(
+            'Movie.title',
+            style: TextStyle(fontSize: 16),
           ),
-          background: FadeInImage(
-           placeholder: AssetImage('assets/loading.gif'),
-           image: NetworkImage('https://via.placeholder.com/500x300'),
-           fit: BoxFit.cover,
-           ),
+        ),
+        background: const FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/500x300'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'),
+              image: NetworkImage('https://via.placeholder.com/200x300'),
+              height: 150,
+            ),
           ),
-        );
+          SizedBox(
+            width: 20,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Modie.title',
+                  style: Theme.of(context).textTheme.headline5,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2),
+              Row(
+                children: [
+                  Icon(Icons.star_outlined, size: 15, color: Colors.grey)
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
--- 5.5.peliculas/peliculas/lib/widgets/card_swipper.dart
+++ 5.5.peliculas/peliculas/lib/widgets/card_swipper.dart
@@ -2,11 +2,8 @@ import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 
 class CardSwiper extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-
-
     final size = MediaQuery.of(context).size;
 
     return Container(
@@ -17,18 +14,19 @@ class CardSwiper extends StatelessWidget {
         layout: SwiperLayout.STACK,
         itemWidth: size.width * 0.6,
         itemHeight: size.height * 0.4,
-        itemBuilder: ( _ , int index) {
-            return GestureDetector(
-              onTap: () => Navigator.pushNamed( _ , 'details', arguments: 'movie-distance'),
-              child: ClipRRect(
-                borderRadius:   BorderRadius.circular(20),
-                child:  FadeInImage(
-                 placeholder: AssetImage('assets/no-image.jpg'),
-                 image: NetworkImage('https://via.placeholder.com/300x400'),
-                 fit: BoxFit.cover,
-                 ),
+        itemBuilder: (_, int index) {
+          return GestureDetector(
+            onTap: () =>
+                Navigator.pushNamed(_, 'details', arguments: 'movie-distance'),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: const FadeInImage(
+                placeholder: AssetImage('assets/no-image.jpg'),
+                image: NetworkImage('https://via.placeholder.com/300x400'),
+                fit: BoxFit.cover,
               ),
-            );
+            ),
+          );
         }, // Significa algo que se construye de manera dinamica
       ),
     );
