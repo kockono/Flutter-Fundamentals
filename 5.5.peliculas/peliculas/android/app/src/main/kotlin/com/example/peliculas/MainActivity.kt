7b0e00e1ad3984da76c32e65dcf25e0f73c4c4ab
kockono <kockonoop@gmail.com>
2022-06-30 01:51:19 -0600

    folders

--- 5.5.peliculas/peliculas/android/app/src/main/kotlin/com/example/peliculas/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.peliculas
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- 5.peliculas/lib/main.dart
+++ 5.peliculas/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:peliculas/src/pages/home_page.dart';
+// Screens
+import 'screens/screens.dart';
 
 
 void main() => runApp(MyApp());
@@ -11,10 +12,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Peliculas',
-      initialRoute: '/',
+      initialRoute: 'home',
       routes: {
-        '/'   : (BuildContext context) => HomePage(),
+        'home': (_) => HomeScreen(),
+        'details': (_) => DetailsScreen(),
       },
+      // theme: ThemeData.light().copyWith(),
+      theme: ThemeData.dark().copyWith(
+        appBarTheme: const AppBarTheme(
+          color: Colors.red
+        )
+      ),
     );
   }
 }
