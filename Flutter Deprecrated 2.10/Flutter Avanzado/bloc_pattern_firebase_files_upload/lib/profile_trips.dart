00b2aaa109a070754a3105979f5f7b2cbb52c2d6
kockono <kockonoop@gmail.com>
2023-05-01 20:42:34 -0600

    Update Flutter 3.17.12

---
+++ Flutter Deprecrated 2.10/Flutter Avanzado/bloc_pattern_firebase_files_upload/lib/profile_trips.dart
@@ -0,0 +1,27 @@
+import 'package:bloc_pattern/profile_header.dart';
+import 'package:flutter/material.dart';
+import 'profile_places_list.dart';
+import 'profile_background.dart';
+
+class ProfileTrips extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    /*return Container(
+      color: Colors.indigo,
+    );*/
+    return Stack(
+      children: <Widget>[
+        ProfileBackground(),
+        ListView(
+          children: <Widget>[
+            ProfileHeader(),
+            ProfilePlacesList()
+
+          ],
+        ),
+      ],
+    );
+  }
+
+}
